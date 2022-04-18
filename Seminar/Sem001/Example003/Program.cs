6ff3d3588de55b97c52aa7f3f621b1dfe5f60c6f
Vadim <vadim.morozov.04021998@yandex.ru>
2022-04-18 23:18:57 +0300

    add Example003

---
+++ Seminar/Sem001/Example003/Program.cs
@@ -0,0 +1,11 @@
+﻿//задача на промежутки
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a - (a * 2);
+int c = b;
+
+while (c <= a)
+{
+    Console.Write(c + ", ");
+    c++;
+}
