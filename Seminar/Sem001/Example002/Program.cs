cbddba0cfa9494401ed1219ff3c707e0d11176c6
Vadim <vadim.morozov.04021998@yandex.ru>
2022-04-18 23:03:00 +0300

    add Example002

--- Seminar/Sem001/Example002/Program.cs
+++ Seminar/Sem001/Example002/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//вывод недели
+Console.WriteLine("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (numA == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (numA == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (numA == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (numA == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (numA == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (numA == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
