9c0c0d7acdd2da710d79c8c1cc7c476ee70a1ab2
Vadim <vadim.morozov.04021998@yandex.ru>
2022-04-18 14:30:49 +0300

    add Seminar

---
+++ Seminar/Sem001/Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 1-е число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numB == numA * numA)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
