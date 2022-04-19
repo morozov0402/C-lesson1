ff5342d41b69cc8e3c8feeb50478516b8afe4165
Vadim <vadim.morozov.04021998@yandex.ru>
2022-04-19 12:58:50 +0300

    Задача на нахождение большего из двух чисел

--- HomeWork/001/Program.cs
+++ HomeWork/001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа на вычисление большего числа
+Console.WriteLine("Введите 1-е число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+if (numberB > numberA)
+{
+    Console.WriteLine("Второе число больше первого");
+}
+if (numberA == numberB)
+{
+    Console.WriteLine("Оба числа равны");
+}
